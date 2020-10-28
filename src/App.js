73a447e84b3403e876e180429e2e527f50132226
Felipe Soto C <pipesoto13@gmail.com>
2020-10-28 18:58:05 -0500

    my solution

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map((element, index) => <li key={index}>{element}</li>)}
         </ul>
       </div>
     );
